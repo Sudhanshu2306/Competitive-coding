9c7e9ff6e6c226838c1fd3226e705e7ad641e445
Sudhanshu2306 <sudhanshuraj2306@gmail.com>
2023-11-23 14:38:20 +0530

    new

---
+++ A_Dubstep.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Commonly used loops
+#define for0(i, n) for (int i = 0; i < (n); ++i)
+#define for1(i, n) for (int i = 1; i <= (n); ++i)
+#define rfor0(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define rfor1(i, n) for (int i = (n); i >= 1; --i)
+
+// Minimum and maximum macros
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+
+// Bit manipulation macros
+#define setBit(x, i) (x |= (1LL << i))
+#define clearBit(x, i) (x &= ~(1LL << i))
+#define toggleBit(x, i) (x ^= (1LL << i))
+#define checkBit(x, i) ((x & (1LL << i)) != 0)
+
+// Shortcuts for common data types
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// 2D vector initialization
+#define vvi(a, m, n, x) vector<vector<int>> a(m, vector<int>(n, x))
+#define vvll(a, m, n, x) vector<vector<ll>> a(m, vector<ll>(n, x))
+
+// map & set
+#define umap unordered_map
+#define umset unordered_set
+
+// Iterate over container elements
+#define foreach(it, v) for (auto it = v.begin(); it != v.end(); ++it)
+#define fori(i, a, b) for (int i = a; i <= b; ++i)
+#define fora(e, v) for (const auto &e : v)
+
+// Floating-point comparisons
+#define EPS 1e-9
+#define EQ(a, b) (abs(a - b) < EPS)
+
+// Debugging macro
+#define debug(x) cerr << #x << " = " << x << endl
+
+// Shortcuts for commonly used functions
+#define gcd __gcd
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+
+
+void solve() {
+    // Your code goes here
+    string s; cin>>s;
+    int n=s.length();
+    string ans;
+    int i=0;
+    int flag=1;
+    while(i<n){
+        if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B' && ans.empty()){
+            i+=3;
+            flag=0;
+        }
+        else if(s[i]=='W' && s[i+1]=='U' && s[i+2]=='B' && !ans.empty()){
+            i+=3;
+            if(flag)
+                ans.push_back(' ');
+            flag=0;
+        }
+        else{
+            ans+=s[i];
+            i++;
+            flag=1;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
+
+
