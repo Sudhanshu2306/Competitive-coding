6bc0cb98a02e5f32467f687db8d98e545a3efb56
Sudhanshu2306 <sudhanshuraj2306@gmail.com>
2023-11-29 23:22:36 +0530

    new questions

---
+++ Monsters.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Commonly used loops
+#define for0(i, n) for (int i = 0; i < (n); ++i)
+#define for1(i, n) for (int i = 1; i <= (n); ++i)
+#define rfor0(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define rfor1(i, n) for (int i = (n); i >= 1; --i)
+
+// Minimum and maximum macros
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+
+// Bit manipulation macros
+#define setBit(x, i) (x |= (1LL << i))
+#define clearBit(x, i) (x &= ~(1LL << i))
+#define toggleBit(x, i) (x ^= (1LL << i))
+#define checkBit(x, i) ((x & (1LL << i)) != 0)
+
+// Shortcuts for common data types
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+// 2D vector initialization
+#define vvi(a, m, n, x) vector<vector<int>> a(m, vector<int>(n, x))
+#define vvll(a, m, n, x) vector<vector<ll>> a(m, vector<ll>(n, x))
+
+// map & set
+#define umap unordered_map
+#define umset unordered_set
+
+// Iterate over container elements
+#define foreach(it, v) for (auto it = v.begin(); it != v.end(); ++it)
+#define fori(i, a, b) for (int i = a; i <= b; ++i)
+#define fora(e, v) for (const auto &e : v)
+
+// Floating-point comparisons
+#define EPS 1e-9
+#define EQ(a, b) (abs(a - b) < EPS)
+
+// Debugging macro
+#define debug(x) cerr << #x << " = " << x << endl
+
+// Shortcuts for commonly used functions
+#define gcd __gcd
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int get_prime(int n) {
+    for (int i = n; i >= 2; --i) {
+        if (n % i == 0 && isPrime(i)) {
+            return i;
+        }
+    }
+    return -1; 
+}
+
+int solve(int h) {
+    int moves = 0;
+    int a = 1;
+    bool flag = false;
+
+    while (h > 0) {
+        if (!flag && isPrime(h)) {
+            int primeFactor = get_prime(h);
+            if (primeFactor == -1) {
+                return -1;
+            }
+            h -= primeFactor;
+            flag = true;
+        } else {
+            
+            h -= a;
+        }
+        a *= 2;
+
+        moves++;
+    }
+
+    if(h==0){
+        return moves;
+    }
+    else{
+        return -1;
+    }
+}
+
+int32_t main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int h;
+        cin >> h;
+
+        int moves = solve(h);
+        cout << moves << endl;
+    }
+
+    return 0;
+}
