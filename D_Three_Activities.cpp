75de432f4efc36484ab42506ca395bb568c9ed64
Sudhanshu2306 <sudhanshuraj2306@gmail.com>
2023-12-19 22:02:20 +0530

    after a while- codeforces

---
+++ D_Three_Activities.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Commonly used loops
+#define for0(i, n) for (int i = 0; i < (n); ++i)
+#define for1(i, n) for (int i = 1; i <= (n); ++i)
+#define rfor0(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define rfor1(i, n) for (int i = (n); i >= 1; --i)
+
+// Minimum and maximum macros
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+
+// Bit manipulation macros
+#define setBit(x, i) (x |= (1LL << i))
+#define clearBit(x, i) (x &= ~(1LL << i))
+#define toggleBit(x, i) (x ^= (1LL << i))
+#define checkBit(x, i) ((x & (1LL << i)) != 0)
+
+// Shortcuts for common data types
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int,int>> vpii;
+// 2D vector initialization
+#define vvi(a, m, n, x) vector<vector<int>> a(m, vector<int>(n, x))
+#define vvll(a, m, n, x) vector<vector<ll>> a(m, vector<ll>(n, x))
+
+// map & set
+#define umap unordered_map
+#define umset unordered_set
+
+// Iterate over container elements
+#define foreach(it, v) for (auto it = v.begin(); it != v.end(); ++it)
+#define fori(i, a, b) for (int i = a; i <= b; ++i)
+#define fora(e, v) for (const auto &e : v)
+
+// Floating-point comparisons
+#define EPS 1e-9
+#define EQ(a, b) (abs(a - b) < EPS)
+
+// Debugging macro
+#define debug(x) cerr << #x << " = " << x << endl
+
+// Shortcuts for commonly used functions
+#define gcd __gcd
+#define lcm(a, b) ((a) / gcd(a, b) * (b))
+
+
+void solve() {
+    // Your code goes here
+    int n; cin>>n;
+    vpii arr(n),brr(n),crr(n);
+
+    for0(i,n){
+        cin>>arr[i].first;
+        arr[i].second=i+1;
+    }
+
+    for0(i,n){
+        cin>>brr[i].first;
+        brr[i].second=i+1;
+    }
+
+    for0(i,n){
+        cin>>crr[i].first;
+        crr[i].second=i+1;
+    }
+
+    sort(arr.begin(),arr.end());
+    reverse(arr.begin(),arr.end());
+    sort(brr.begin(),brr.end());
+    reverse(brr.begin(),brr.end());
+    sort(crr.begin(),crr.end());
+    reverse(crr.begin(),crr.end());
+
+    int ans=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            for(int k=0;k<3;k++){
+                if(arr[i].second != brr[j].second && arr[i].second!=crr[k].second && brr[j].second != crr[k].second){
+                    ans=max(ans,arr[i].first+brr[j].first+crr[k].first);
+                }
+            }
+        }
+    }
+
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
+
+
